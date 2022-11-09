ef00c312e07720a350cfb353e677982f5b2d5aa3
Noah Loomis <nloomis@spiria.com>
2022-11-09 10:36:13 -0500

    ba

--- components/Wrapper.tsx
+++
@@ -1,7 +0,0 @@
-const Wrapper = ({ children }: { children: JSX.Element }) => (
-  <div className="box-border flex max-lg  justify-center px-12 py-16 mt-4">
-    {children}
-  </div>
-);
-
-export default Wrapper;
