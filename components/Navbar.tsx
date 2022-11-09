146f79c9074e6472c6d469563317c1b0594fbe59
Noah Loomis <nloomis@spiria.com>
2022-11-09 10:33:56 -0500

    finale

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,4 @@
+import {useState} from 'react'
 import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
 import { useRouter } from "next/router";
 import Link from "next/link";
@@ -14,13 +15,36 @@ const Navbar = () => {
 
   return (
     <div className="navbar text-white bg-slate-600">
-      <div className="navbar-start flex">
-          <Link href="/articles" className="btn btn-ghost mx-2">
-            View Articles
-          </Link>
-          <Link href="/articles/create" className="btn btn-ghost mx-2">
-            Create Articles
-          </Link>
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost btn-circle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h7"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-slate-600 rounded-box w-52"
+          >
+            <li>
+              <Link href="/articles">View Articles</Link>
+            </li>
+            <li>
+              <Link href="/articles/create">Create Articles</Link>
+            </li>
+          </ul>
+        </div>
       </div>
       <div className="navbar-center">
         <Link href="/" className="btn btn-ghost normal-case text-xl">
